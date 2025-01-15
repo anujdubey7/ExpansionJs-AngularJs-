ea43f293c4ebb6c01791aaa3b7c04d5b1dc49384
anujdubey7 <anuj.dubey@expansionjs.com>
2025-01-15 12:25:13 +0000

    main

--- src/app/service/deact-guard.guard.ts
+++ src/app/service/deact-guard.guard.ts
@@ -4,6 +4,6 @@ import { CanDeactivateFn } from '@angular/router';
 
 export const deactGuardGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
 
-  
+  //check form-deactivate-class.guard.ts file 
   return true;
 };
